5f24a74ae538979c48d259c781e8d8eae346304d
Aishwary Shukla <aishwary.shukla@quantiphi.com>
2025-12-03 14:27:58 -0500

    response function addeD

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 from module.bls.ingest import BLSIngest
 from module.datausa.ingest import DataUsaIngest
-from flask import Flask
+from flask import (
+    Flask,
+    make_response
+)
 import logging
 import yaml
 
@@ -14,34 +17,40 @@ with open("config.yaml", "r") as fp:
 
 @app.route("/ingest/bls/timeseries/<string:section>", methods=["GET"])
 def ingest_bls_data(section):
-    bls_config = app_config["bls"]
-    bls_ingestor = BLSIngest(
-        base_url=bls_config["timeseries"]["base_url"],
-        section=section,
-        files_to_skip=bls_config["timeseries"][section]["files_to_skip"],
-        metadata_tbl=(
-            f"{bls_config['metadata']['bq_dataset']}."
-            f"{bls_config['metadata']['bq_table']}"
-        ),
-        bq_project_id=bls_config['metadata']['bq_project'],
-        storage_bkt=bls_config["timeseries"]['storage_bkt']
-    )
-    bls_ingestor.ingest()
-    return "200"
+    try:
+        bls_config = app_config["bls"]
+        bls_ingestor = BLSIngest(
+            base_url=bls_config["timeseries"]["base_url"],
+            section=section,
+            files_to_skip=bls_config["timeseries"][section]["files_to_skip"],
+            metadata_tbl=(
+                f"{bls_config['metadata']['bq_dataset']}."
+                f"{bls_config['metadata']['bq_table']}"
+            ),
+            bq_project_id=bls_config['metadata']['bq_project'],
+            storage_bkt=bls_config["timeseries"]['storage_bkt']
+        )
+        bls_ingestor.ingest()
+    except Exception as exc:
+        return make_response("Error! Please investigate", 500)    
+    return make_response("Ingestion Complete", 200)
 
 
 @app.route("/ingest/datausa/<string:region_name>", methods=["GET"])
 def ingest_honolulu_datasusa(region_name):
-    region_name = region_name.lower()
-    datausa_config = app_config["api"]["datausa"]
-    DataUsaIngest(
-        region=region_name.lower()
-        storage_bkt=datausa_config["storage_bkt"]
-        base_url=datausa_config[region_name]["base_url"]
-        resource_path=datausa_config[region_name]["resource_path"]
-        query_params=datausa_config[region_name]["query_params"]
-    ).ingest()
-    return 200
+    try:
+        region_name = region_name.lower()
+        datausa_config = app_config["api"]["datausa"]
+        DataUsaIngest(
+            region=region_name,
+            storage_bkt=datausa_config["storage_bkt"],
+            base_url=datausa_config[region_name]["base_url"],
+            resource_path=datausa_config[region_name]["resource_path"],
+            query_params=datausa_config[region_name]["query_params"]
+        ).ingest()
+    except Exception as exc:
+        return make_response("Error! Please investigate", 500)
+    return make_response("Ingestion Complete", 200)
 
 
 if __name__ == "__main__":
